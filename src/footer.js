2ab48dddb58e636e87f5e77daf734c982ca97c11
Jesse Shaw <JesseShawCodes@gmail.com>
2018-10-01 13:36:09 -0400

    Updated Footer

--- src/footer.js
+++ src/footer.js
@@ -4,7 +4,21 @@ class Banner extends Component {
     render() {
         return (
             <footer>
-                <h1>Footer</h1>
+                <div className="footer-header">
+                    <h2>Questions about the App?</h2>
+                </div>
+                <div className="contact">
+                    <h3>If you have any questions regarding this project, feel free to contact Jesse Shaw at any of the points of contact below:</h3>
+                    <div className="contact-tel">
+                        410.703.6125
+                    </div>
+                    <div className="contact-email">
+                        JesseShawCodes@gmail.com
+                    </div>
+                    <div className="contact-github">
+                        GitHub
+                    </div>
+                </div>
             </footer>
         );
     }
